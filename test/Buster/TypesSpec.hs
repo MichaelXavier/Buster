0131a212b97eeefc375570db73bfb69eae82c139
Michael Xavier <michael@michaelxavier.net>
2012-12-08 12:31:59 -0800

    More complete testing

--- test/Buster/TypesSpec.hs
+++ test/Buster/TypesSpec.hs
@@ -15,6 +15,10 @@ spec = do
       parseStr baseConfigStr `shouldBe` Right baseConfig
     it "parses a full Config" $ 
       parseStr fullConfigStr `shouldBe` Right fullConfig
+    it "defaults method" $
+      parseStr fullConfigNoMethodStr `shouldBe` Right fullConfigGet
+    it "parses multiple urls" $
+      parseStr fullConfigMultipleStr `shouldBe` Right fullConfigMultiple
   where baseConfigStr = [s|
 ---
 urls: []
@@ -26,9 +30,30 @@ urls:
   interval: 1000
   method: POST
         |]
+        fullConfigNoMethodStr = [s|
+verbose: true
+urls:
+- url: http://www.example.com
+  interval: 1000
+        |]
+        fullConfigMultipleStr = [s|
+verbose: true
+urls:
+- url: http://www.example.com
+  interval: 1000
+  method: POST
+- url: http://www.example.com
+  interval: 1000
+  method: GET
+        |]
         baseConfig = Config { configVerbose = False, urlConfigs = []}
+        fullConfigGet = Config { configVerbose = True,
+                                 urlConfigs = [fullUrlConfig { requestMethod = "GET"}]}
         fullConfig = Config { configVerbose = True,
                               urlConfigs = [fullUrlConfig]}
+        fullConfigMultiple = Config { configVerbose = True,
+                                      urlConfigs = [fullUrlConfig,
+                                                    fullUrlConfig { requestMethod = "GET"}]}
         fullUrlConfig = UrlConfig { url             = "http://www.example.com",
                                     requestInterval = 1000,
                                     requestMethod   = "POST" }
