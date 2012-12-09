34f9b95e0e88fd44923160594415595cf5670bb0
Michael Xavier <michael@michaelxavier.net>
2012-12-09 00:54:51 -0800

    Switch to logger in stub requester

--- src/Buster/Request.hs
+++ src/Buster/Request.hs
@@ -1,8 +1,9 @@
 module Buster.Request (makeRequest) where
 
 import Buster.Types
+import Buster.Logger
 import Network.HTTP.Conduit (Manager)
 
 -- todo: logger, request manager
 makeRequest :: Manager -> UrlConfig -> IO ()
-makeRequest mgr urlConfig = putStrLn $ "WOULD REQUEST " ++ url urlConfig
+makeRequest mgr urlConfig = debugM $ "Would request " ++ url urlConfig
