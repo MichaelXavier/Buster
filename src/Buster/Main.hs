d2eb89bd28bedb6ae51fce8eda5e8ee0527bcafe
Michael Xavier <michael@michaelxavier.net>
2012-12-09 19:42:05 -0800

    delay indefinitely instead of using mvar

--- src/Buster/Main.hs
+++ src/Buster/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
 import Control.Applicative ((<$>))
-import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 import Data.Maybe (listToMaybe)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
@@ -15,25 +16,24 @@ import Buster.Logger
 --TODO: use eitherT
 main :: IO ()
 main = do configFile <- listToMaybe <$> getArgs
-          exitSig    <- maybe noConfigError runWithPath configFile
+          maybe noConfigError runWithPath configFile
+          debugM "Sleeping main thread indefinitely"
+          forever $ threadDelay maxBound
           --TODO: debug the blocked indefinitely on MVar error
-          takeMVar exitSig
   where noConfigError = failWith "Specify config file" >> error "nah dude"
 
 
-runWithPath :: FilePath -> IO (MVar ())
+runWithPath :: FilePath -> IO ()
 runWithPath path = either badConfigError run =<< loadConfig path
   where badConfigError msg = failWith msg >> error "nah dude"
 
 -- TODO: trap sigints and exit successfully
-run :: Config -> IO (MVar ())
+run :: Config -> IO ()
 run cfg = do configureLogger $ configVerbose cfg
              infoM "Starting Buster"
              pool <- newPool cfg
              startPool pool
              infoM "Pool started"
-             newEmptyMVar
-
 
 failWith :: String -> IO ()
 failWith msg = hPutStrLn stderr msg >> exitFailure
